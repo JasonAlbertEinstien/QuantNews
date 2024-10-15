a792e80314831d5b07429e313239e11e8f645107
JasonAlbertEinstien <j2004nol@gmail.com>
2024-10-15 13:40:37 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+API_KEY:str = ""
+
+
+class WebObj:
+    def __init__(self): 
+        pass
+
+    def get():
+        pass
+
+
+class FredData:
+    def __init__(self):
+        pass
+
+        
+    
